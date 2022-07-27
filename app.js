f0aa59b58e2f5de922b57b1a697f93b2d7b69927
Volodimir <vova19941@ukr.net>
2022-07-27 20:24:03 +0300

    add

--- app.js
+++ app.js
@@ -1,43 +1,53 @@
-let productsCount = document.getElementById("products-count");
-console.log(productsCount);
+// let productsCount = document.getElementById("products-count");
+// console.log(productsCount);
 
-let addToCartBtns = document.querySelectorAll(".btn-add-to-cart");
-console.log(addToCartBtns);
+// let addToCartBtns = document.querySelectorAll(".btn-add-to-cart");
+// console.log(addToCartBtns);
 
-for (let i = 0; i < addToCartBtns.length; i++) {
-  addToCartBtns[i].addEventListener("click", function () {
-    // перший варыант
-    // let prevProductsCount = +productsCount.textContent;
-    // productsCount.textContent = prevProductsCount + 1;
-    // другий варіант
-    productsCount.textContent = +productsCount.textContent + 1;
-  });
-}
+// for (let i = 0; i < addToCartBtns.length; i++) {
+//   addToCartBtns[i].addEventListener("click", function () {
+//     // перший варыант
+//     // let prevProductsCount = +productsCount.textContent;
+//     // productsCount.textContent = prevProductsCount + 1;
+//     // другий варіант
+//     productsCount.textContent = +productsCount.textContent + 1;
+//   });
+// }
 
-// addToCartBtns.forEach((item) =>
+// // addToCartBtns.forEach((item) =>
+// //   item.addEventListener("click", function () {
+// //     // productsCount.innerText =
+// //     //   productsCount.counter >= 1
+// //     //     ? ++productsCount.counter
+// //     //     : (productsCount.counter = 1);
+
+// //     console.log("clicked forEach");
+// //   })
+// // );
+
+// //змінити лайки//
+
+// let likeImg = document.querySelectorAll(".like-img");
+
+// console.log(likeImg);
+
+// likeImg.forEach((item) =>
 //   item.addEventListener("click", function () {
-//     // productsCount.innerText =
-//     //   productsCount.counter >= 1
-//     //     ? ++productsCount.counter
-//     //     : (productsCount.counter = 1);
-
-//     console.log("clicked forEach");
+//     // if (item.classList.contains("like-img")) {
+//     //   item.classList.remove("like-img");
+//     // } else {
+//     //   item.classList.add("like-img");
+//     // }
+//     item.classList.toggle("liked");
 //   })
 // );
 
-//змінити лайки//
+//btn-more-details//
+let moreDetailsBtns = document.querySelectorAll(".btn-more-details");
+let modal = document.querySelector(".modal");
 
-let likeImg = document.querySelectorAll(".like-img");
+moreDetailsBtns.forEach(item) => item.addEventListener("click",function(){
+  modal.classList.add("show");
+})
 
-console.log(likeImg);
 
-likeImg.forEach((item) =>
-  item.addEventListener("click", function () {
-    // if (item.classList.contains("like-img")) {
-    //   item.classList.remove("like-img");
-    // } else {
-    //   item.classList.add("like-img");
-    // }
-    item.classList.toggle("liked");
-  })
-);
