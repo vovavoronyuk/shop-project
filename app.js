9b8bdeab06b65ad88c9971910b9b49952d967cf8
Volodimir <vova19941@ukr.net>
2022-07-27 18:58:05 +0300

    кнопка

--- app.js
+++ app.js
@@ -6,19 +6,21 @@ console.log(addToCartBtns);
 
 for (let i = 0; i < addToCartBtns.length; i++) {
   addToCartBtns[i].addEventListener("click", function () {
-    let btn = addToCartBtns[i];
-
-    console.log("clicked");
+    // перший варыант
+    // let prevProductsCount = +productsCount.textContent;
+    // productsCount.textContent = prevProductsCount + 1;
+    // другий варіант
+    productsCount.textContent = +productsCount.textContent + 1;
   });
 }
 
-addToCartBtns.forEach((item) =>
-  item.addEventListener("click", function () {
-    productsCount.innerText =
-      productsCount.counter >= 1
-        ? ++productsCount.counter
-        : (productsCount.counter = 1);
+// addToCartBtns.forEach((item) =>
+//   item.addEventListener("click", function () {
+//     // productsCount.innerText =
+//     //   productsCount.counter >= 1
+//     //     ? ++productsCount.counter
+//     //     : (productsCount.counter = 1);
 
-    console.log("clicked forEach");
-  })
-);
+//     console.log("clicked forEach");
+//   })
+// );
