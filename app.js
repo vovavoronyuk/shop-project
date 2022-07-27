f131f0267f5b87b350500631991edff1c4173712
Volodimir <vova19941@ukr.net>
2022-07-27 14:53:49 +0300

    click

--- app.js
+++ app.js
@@ -6,12 +6,19 @@ console.log(addToCartBtns);
 
 for (let i = 0; i < addToCartBtns.length; i++) {
   addToCartBtns[i].addEventListener("click", function () {
+    let btn = addToCartBtns[i];
+
     console.log("clicked");
   });
 }
 
 addToCartBtns.forEach((item) =>
   item.addEventListener("click", function () {
+    productsCount.innerText =
+      productsCount.counter >= 1
+        ? ++productsCount.counter
+        : (productsCount.counter = 1);
+
     console.log("clicked forEach");
   })
 );
