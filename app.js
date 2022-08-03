f21cd4df2fe63f4efb35d75276caaede917e9428
Volodimir <vova19941@ukr.net>
2022-08-03 17:14:34 +0300

    js

--- app.js
+++ app.js
@@ -132,8 +132,8 @@ if (counters) {
           --value;
         }
 
-        if (value <= 0) {
-          value = 0;
+        if (value <= 1) {
+          value = 1;
           target
             .closest(".counter")
             .querySelector(".counter_button_minus")
